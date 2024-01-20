563e1c5dfd9da5c610a1c4765b05dc2c1bfc1df7
mfedoriv <mfedorivs@gmail.com>
2024-01-20 16:09:19 +0300

    Initial commit

---
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Pressing W!");
+        }
+        else
+        {
+            Debug.Log("-");
+        }
+    }
+}
