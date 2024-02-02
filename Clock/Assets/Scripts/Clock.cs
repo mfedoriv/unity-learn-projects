1912e22e8d074f8b6dd798328d5fa23e3978fb94
mfedoriv <mfedorivs@gmail.com>
2024-02-02 22:37:22 +0300

    Implemented clock animation displaying current system time

--- Clock/Assets/Scripts/Clock.cs
+++ Clock/Assets/Scripts/Clock.cs
@@ -1,6 +1,19 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Clock : MonoBehaviour
 {
-    [SerializeField] private Transform _hoursPivot, _minutesPivot, seconds_Pivot;
+    private const float HoursToDegrees = -30f, MinutesToDegrees = -6f, SecondsToDegrees = -6f;
+    
+    [SerializeField] private Transform hoursPivot, minutesPivot, secondsPivot;
+    
+
+    private void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, HoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, MinutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, SecondsToDegrees * (float)time.TotalSeconds);
+    }
 }
