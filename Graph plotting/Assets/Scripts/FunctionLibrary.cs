5ca1a05d335246ce567d1656fc6098787dba82bb
mfedoriv <mfedorivs@gmail.com>
2024-02-17 17:24:35 +0300

    Graph plotting. Added 3D functions and updated visualisations process

--- Graph plotting/Assets/Scripts/FunctionLibrary.cs
+++ Graph plotting/Assets/Scripts/FunctionLibrary.cs
@@ -3,39 +3,79 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public delegate float Function(float x, float z, float t);
+    public delegate Vector3 Function(float u, float v, float t);
     
-    public enum FunctionName { Wave, MultiWave, Ripple }
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
 
-    private static readonly Function[] Functions = {Wave, MultiWave, Ripple};
+    private static readonly Function[] Functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
     public static Function GetFunction(FunctionName name)
     {
         return Functions[(int)name];
     }
 
-    private static float Wave(float x, float z, float t)
+    private static Vector3 Wave(float u, float v, float t)
     {
-        return Sin(PI * (x + z + t));
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
 
-    private static float MultiWave(float x, float z, float t)
+    private static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + 0.5f * t));
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
         // Division requires a bit more work than multiplication, so it's a rule of thumb to prefer multiplication over
         // division. However, constant expressions like 1f / 2f and also 2f * Mathf.PI are already reduced to a single
         // number by the compiler.
         // y += Sin(2f * PI * (x + t)) / 2f; 
-        y += 0.5f * Sin(2f * PI * (z + t));
-        y += Sin(PI * (x + z + 0.25f * t));
-        return y * (1f / 2.5f);
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= (1f / 2.5f);
+        p.z = v;
+        return p;
     }
 
-    private static float Ripple(float x, float z, float t)
+    private static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Sqrt(x * x + z * z);
-        float y = Sin(PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= (1f + 10f * d);
+        p.z = v;
+        return p;
     }
-    
+
+
+    private static Vector3 Sphere(float u, float v, float t)
+    {
+        // float r = 0.5f + 0.5f * Sin(PI * t); // Scale sphere
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t)); // Vary uniform radius
+        float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
+
+        return p;
+    }
+
+
+    private static Vector3 Torus(float u, float v, float t)
+    {
+        // float r1 = 0.75f; // simple torus
+        // float r2 = 0.25f;
+        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+        float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+        float s = r1 + r2 * Cos(PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r2 * Sin(PI * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
 }
--- Graph plotting/Assets/Scripts/Graph.cs
+++ Graph plotting/Assets/Scripts/Graph.cs
@@ -14,20 +14,11 @@ public class Graph : MonoBehaviour
     private void Awake()
     {
         float step = 2f / resolution;
-        var position = Vector3.zero;
         var scale = Vector3.one * step;
         _points = new Transform[resolution * resolution];
-        for (int i = 0, x = 0, z = 0; i < _points.Length; i++, x++)
+        for (int i = 0; i < _points.Length; i++)
         {
-            if (x == resolution)
-            {
-                x = 0;
-                z += 1;
-            }
             Transform point = _points[i] = Instantiate(pointPrefab, transform, false);
-            position.x = (x + 0.5f) * step - 1f; // filling (-1,1) range
-            position.z = (z + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
         }
 
@@ -37,13 +28,19 @@ public class Graph : MonoBehaviour
     {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for (int i = 0; i < _points.Length; i++)
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < _points.Length; i++, x++)
         {
-            Transform point = _points[i];
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
             
-            point.localPosition = position;
+            _points[i].localPosition = f(u, v, time);
 
         }
     }
