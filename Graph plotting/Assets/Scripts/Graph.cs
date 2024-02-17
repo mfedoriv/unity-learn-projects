4125c7a502630328dda2cff6dd05e0ff06e8888e
mfedoriv <mfedorivs@gmail.com>
2024-02-17 12:53:19 +0300

    Graph plotting. Added URP coloring by position shader. Animated sine wave

--- Graph plotting/Assets/Scripts/Graph.cs
+++ Graph plotting/Assets/Scripts/Graph.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
@@ -6,19 +7,33 @@ public class Graph : MonoBehaviour
 
     [SerializeField, Range(10, 100)] private int resolution = 10;
 
+    private Transform[] _points;
+
     private void Awake()
     {
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
+        _points = new Transform[resolution];
+        for (int i = 0; i < _points.Length; i++)
         {
-            Transform point = Instantiate(pointPrefab, transform, false);
+            Transform point = _points[i] = Instantiate(pointPrefab, transform, false);
             position.x = (i + 0.5f) * step - 1f; // filling (-1,1) range
-            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
         }
 
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Transform point = _points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+
+        }
     }
 }
--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -5,21 +5,96 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameInput gameInput;
-    
     [SerializeField] private float maxSpeed = 7f;
     [SerializeField] private float acceleration = 45f;
     [SerializeField] private float deceleration = 20f;
-
+    [SerializeField] private LayerMask countersLayerMask;
+    
     private bool _isWalking;
     private Vector3 _currentVelocity;
+    private Vector3 _lastInteractDirection;
     
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        HandleMovement(moveDirection);
+        if (moveDirection != Vector3.zero)
+        {
+            _lastInteractDirection = moveDirection;
+        }
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, _lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) 
+             {
+                clearCounter.Interact();
+             }
+        }
+        else
+        {
+            Debug.Log("-");
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = maxSpeed * Time.deltaTime;
+        float playerRadius = .65f;
+        float playerHeight = 2f;
+        var position = transform.position;
+        bool canMove = !Physics.CapsuleCast(position, position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        
+        // This code is very confusing. Do i really need to do this with a capsule cast or i can do this with usual collider?
+        // UPD: Actually, this is some like of Collide and Slide algorithm
+        // more about: https://www.youtube.com/watch?v=YR6Q7dUz2uk&t=617s
+        if (!canMove)
+        {
+            // If can't move towards moveDirection (collisions detection)
+            // Attempt  only X movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(position, position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove)
+            { 
+                // Can move only on X
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                // Can't move only on X
+                // Attempt only Z movement
+
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(position, position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+                if (canMove)
+                {
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    // Can't move in any direction
+                }
+
+            }
+        }
+        if (canMove)
+        {
+            // HandleKinematicMovement(moveDirection); // Should be updated in order to work with player's collisions
+            transform.position += moveDirection * moveDistance;
+        }
+
 
         _isWalking = moveDirection != Vector3.zero;
         
@@ -29,7 +104,7 @@ public class Player : MonoBehaviour
         // Debug.Log(Time.deltaTime);
     }
 
-    private void HandleMovement(Vector3 moveDirection)
+    private void HandleKinematicMovement(Vector3 moveDirection)
     {
         if (moveDirection != Vector3.zero)
         {
