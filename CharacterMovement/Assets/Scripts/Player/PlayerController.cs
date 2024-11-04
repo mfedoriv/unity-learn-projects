594d4ea72a56835c793f9d32361986d564dbd148
mfedoriv <mfedorivs@gmail.com>
2024-11-04 22:02:42 +0300

    PlayerMovement: Changed Input Handling methods

--- CharacterMovement/Assets/Scripts/Player/PlayerController.cs
+++ CharacterMovement/Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using ECM2;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Player
 {
@@ -19,11 +20,15 @@ namespace Player
         [Tooltip("How far in degrees can you move the camera down.")]
         [SerializeField]
         private float minPitch = -80.0f;
+        
+        [Space(15.0f)] [Tooltip("Mouse look sensitivity")]
+        [SerializeField]
+        private Vector2 mouseSensitivity = new Vector2(1.5f, 1.25f);
 
         [Space(15.0f)] [Tooltip("Cinemachine Virtual Camera positioned at desired crouched height.")]
         [SerializeField]
         private CinemachineVirtualCamera crouchedCamera;
-
+        
         [Tooltip("Cinemachine Virtual Camera positioned at desired un-crouched height.")]
         [SerializeField]
         private CinemachineVirtualCamera unCrouchedCamera;
@@ -32,21 +37,11 @@ namespace Player
         [SerializeField]
         private float amplitudeMultiplier = 1f;
         
-        [Space(15.0f)] [Tooltip("Mouse look sensitivity")]
-        [SerializeField]
-        private Vector2 lookSensitivity = new Vector2(1.5f, 1.25f);
-        
         private CinemachineBasicMultiChannelPerlin _unCrouchedNoiseProfile;
-        
         private CinemachineBasicMultiChannelPerlin _crouchedNoiseProfile;
-        
-        // Cached Character
-
         private Character _character;
-
-        // Current camera target pitch
-
         private float _cameraTargetPitch;
+        private PlayerInputHandler _inputHandler;
 
         private void Awake()
         {
@@ -61,6 +56,8 @@ namespace Player
             {
                 _crouchedNoiseProfile = crouchedCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
             }
+            
+            _inputHandler = PlayerInputHandler.Instance;
         }
 
         private void Start()
@@ -90,111 +87,73 @@ namespace Player
 
         private void Update()
         {
-            // HandleMovement();
-            // HandleCameraRotation();
+            HandleMovement();
+            HandleRotation();
             UpdateNoiseAmplitude();
         }
 
-        public void OnMove(InputAction.CallbackContext context)
+        private void HandleMovement()
         {
-            // Read input values
-        
-            Vector2 moveInput = context.ReadValue<Vector2>();
-        
-            // Compose a movement direction vector in world space
-        
-            Vector3 movementDirection = Vector3.zero;
-        
-            movementDirection += Vector3.forward * moveInput.y;
-            movementDirection += Vector3.right * moveInput.x;
-        
-            // If character has a camera assigned,
-            // make movement direction relative to this camera view direction
-        
-            if (_character.camera)
-            {               
-                movementDirection = movementDirection.relativeTo(_character.cameraTransform);
-                Debug.Log($"Camera Transform: {_character.cameraTransform.rotation}, {context.phase}");
-            }
-        
-            // Set character's movement direction vector
-        
-            _character.SetMovementDirection(movementDirection);
-        }
-        
-        // public void OnMove(InputAction.CallbackContext context)
-        // {
-        //     Vector2 moveInput = context.ReadValue<Vector2>();
-        //
-        //     // Получаем направление камеры
-        //     Vector3 forward = cameraTarget.transform.forward;
-        //     Vector3 right = cameraTarget.transform.right;
-        //
-        //     // Проекция на плоскость, убираем компонент по оси Y
-        //     forward.y = 0;
-        //     right.y = 0;
-        //
-        //     forward.Normalize();
-        //     right.Normalize();
-        //
-        //     // Вычисляем конечное направление движения
-        //     Vector3 movementDirection = forward * moveInput.y + right * moveInput.x;
-        //
-        //     // Задаем направление движения персонажа
-        //     _character.SetMovementDirection(movementDirection);
-        // }
-        
-        public void OnLook(InputAction.CallbackContext context)
-        {
-            Vector2 lookInput = context.ReadValue<Vector2>() * lookSensitivity * Time.deltaTime;
-            print($"Look Input: {lookInput}, Context {context.phase}, Value from context {context.ReadValue<Vector2>()}");
-            AddControlYawInput(lookInput.x);
-            AddControlPitchInput(lookInput.y, minPitch, maxPitch);
-        }
-        
+            Vector3 inputDirection = new Vector3(_inputHandler.MoveInput.x, 0f, _inputHandler.MoveInput.y);
+            Vector3 worldDirection = transform.TransformDirection(inputDirection);
+            worldDirection.Normalize();
 
-        // private void HandleCameraRotation()
-        // {
-        //     Vector2 lookInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        //     AddControlYawInput(lookInput.x * lookSensitivity.x);
-        //     AddControlPitchInput(lookInput.y * lookSensitivity.y, minPitch, maxPitch);
-        // }
-
-        public void OnCrouch(InputAction.CallbackContext context)
-        {
-            if (context.started)
-                _character.Crouch();
-            else if (context.canceled)
-                _character.UnCrouch();
-        }
-        
-        public void OnJump(InputAction.CallbackContext context)
-        {
-            if (context.started)
-                _character.Jump();
-            else if (context.canceled)
-                _character.StopJumping();
+            HandleJumping();
+            HandleCrouching();
+            _character.SetMovementDirection(worldDirection);
         }
 
-        private void UpdateNoiseAmplitude()
+        private void HandleJumping()
         {
-            if (_unCrouchedNoiseProfile == null && _crouchedNoiseProfile == null)
-                return;
-
-            float currentSpeed = _character.velocity.magnitude;
-            
-            if (_character.IsCrouched())
+            if (_inputHandler.JumpTriggered)
             {
-                float speedRatio = Mathf.Clamp01(currentSpeed / _character.maxWalkSpeed);
-                _crouchedNoiseProfile.m_AmplitudeGain = speedRatio * amplitudeMultiplier;
+                _character.Jump();
             }
             else
             {
-                float speedRatio = Mathf.Clamp01(currentSpeed / _character.maxWalkSpeedCrouched);
-                _unCrouchedNoiseProfile.m_AmplitudeGain = speedRatio * amplitudeMultiplier;
+                _character.StopJumping();
             }
         }
 
+        private void HandleCrouching()
+        {
+            if (_inputHandler.CrouchTriggered)
+            {
+                _character.Crouch();
+            }
+            else
+            {
+                _character.UnCrouch();
+            }
+        }
+        
+        /// <summary>
+        /// When character crouches, toggle Crouched / UnCrouched cameras.
+        /// </summary>
+        private void OnCrouched()
+        {
+            crouchedCamera.Priority = 11;
+            unCrouchedCamera.Priority = 10;
+        }
+
+        /// <summary>
+        /// When character un-crouches, toggle Crouched / UnCrouched cameras.
+        /// </summary>
+        private void OnUnCrouched()
+        {
+            crouchedCamera.Priority = 10;
+            unCrouchedCamera.Priority = 11;
+        }
+        
+        private void HandleRotation()
+        {
+            float mouseXRotation = _inputHandler.LookInput.x * mouseSensitivity.x;
+            AddControlYawInput(mouseXRotation);
+
+            float mouseYRotation = _inputHandler.LookInput.y * mouseSensitivity.y;
+            AddControlPitchInput(mouseYRotation, minPitch, maxPitch);
+        }
+        
         /// <summary>
         /// Add input (affecting Yaw).
         /// This is applied to the Character's rotation.
@@ -217,22 +176,23 @@ namespace Player
             cameraTarget.transform.localRotation = Quaternion.Euler(-_cameraTargetPitch, 0.0f, 0.0f);
         }
 
-        /// <summary>
-        /// When character crouches, toggle Crouched / UnCrouched cameras.
-        /// </summary>
-        private void OnCrouched()
+        private void UpdateNoiseAmplitude()
         {
-            crouchedCamera.Priority = 11;
-            unCrouchedCamera.Priority = 10;
-        }
+            if (_unCrouchedNoiseProfile == null && _crouchedNoiseProfile == null)
+                return;
 
-        /// <summary>
-        /// When character un-crouches, toggle Crouched / UnCrouched cameras.
-        /// </summary>
-        private void OnUnCrouched()
-        {
-            crouchedCamera.Priority = 10;
-            unCrouchedCamera.Priority = 11;
+            float currentSpeed = _character.velocity.magnitude;
+            
+            if (_character.IsCrouched())
+            {
+                float speedRatio = Mathf.Clamp01(currentSpeed / _character.maxWalkSpeed);
+                _crouchedNoiseProfile.m_AmplitudeGain = speedRatio * amplitudeMultiplier;
+            }
+            else
+            {
+                float speedRatio = Mathf.Clamp01(currentSpeed / _character.maxWalkSpeedCrouched);
+                _unCrouchedNoiseProfile.m_AmplitudeGain = speedRatio * amplitudeMultiplier;
+            }
         }
     }
 }
---
+++ CharacterMovement/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    [Header("Input Action Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name References")]
+    [SerializeField] private string actionMapName = "Player";
+    
+    [Header("Action Name References")]
+    [SerializeField] private string move = "Move";
+    [SerializeField] private string look = "Look";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string crouch = "Crouch";
+
+    private InputAction _moveAction;
+    private InputAction _lookAction;
+    private InputAction _jumpAction;
+    private InputAction _crouchAction;
+    
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LookInput { get; private set; }
+    public bool JumpTriggered { get; private set; }
+    public bool CrouchTriggered { get; private set; }
+    
+    public static PlayerInputHandler Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _moveAction = playerControls.FindActionMap(actionMapName).FindAction(move);
+        _lookAction = playerControls.FindActionMap(actionMapName).FindAction(look);
+        _jumpAction = playerControls.FindActionMap(actionMapName).FindAction(jump);
+        _crouchAction = playerControls.FindActionMap(actionMapName).FindAction(crouch);
+        RegisterInputActions();
+    }
+
+    private void RegisterInputActions()
+    {
+        _moveAction.performed += context => MoveInput = context.ReadValue<Vector2>();
+        _moveAction.canceled += context => MoveInput = Vector2.zero;
+
+        _lookAction.performed += context => LookInput = context.ReadValue<Vector2>();
+        _lookAction.canceled += context => LookInput = Vector2.zero;
+
+        _jumpAction.performed += context => JumpTriggered = true;
+        _jumpAction.canceled += context => JumpTriggered = false;
+
+        _crouchAction.performed += context => CrouchTriggered = true;
+        _crouchAction.canceled += context => CrouchTriggered = false;
+    }
+
+    private void OnEnable()
+    {
+        _moveAction.Enable();
+        _lookAction.Enable();
+        _jumpAction.Enable();
+        _crouchAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.Disable();
+        _lookAction.Disable();
+        _jumpAction.Disable();
+        _crouchAction.Disable();
+    }
+}
